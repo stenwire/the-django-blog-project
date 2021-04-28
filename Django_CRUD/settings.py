0b868e0742f70aa33f506e3e7323b381b425576d
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2021-04-28 08:29:13 +0100

    Update settings.py

--- Django_CRUD/settings.py
+++ Django_CRUD/settings.py
@@ -124,11 +124,11 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGIN_REDIRECT_URL="blog:home"
 LOGOUT_REDIRECT_URL="blog:home"
