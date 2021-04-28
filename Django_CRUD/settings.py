ef06387b1c06c4d6bc0102885fff030b37e550f7
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2021-04-28 08:26:34 +0100

    Update settings.py

--- Django_CRUD/settings.py
+++ Django_CRUD/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL="blog:home"
 LOGOUT_REDIRECT_URL="blog:home"
